89ab7ac74b3c6e362ed96bd85590f34b536ed6fa
takuya-ichikawa <ichikawatakuyadesu@gmail.com>
2026-03-01 13:50:25 +0000

    api-typesのmainとtypesのパスをsrcに更新

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 export default {
   reactStrictMode: true,
+  transpilePackages: ['api-types'],
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
