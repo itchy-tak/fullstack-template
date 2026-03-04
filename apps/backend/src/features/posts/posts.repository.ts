0df0c353d11c8d9402e5dd6ffabe7d1bcba01e1d
takuya-ichikawa <ichikawatakuyadesu@gmail.com>
2026-03-04 13:09:58 +0000

    backendのtsconfigのパスエイリアスを無効化

--- apps/backend/src/features/posts/posts.repository.ts
+++ apps/backend/src/features/posts/posts.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Post, Prisma } from '@prisma/client';
 
-import { PrismaService } from '@/common/prisma/prisma.service';
+import { PrismaService } from '../../common/prisma/prisma.service';
 
 @Injectable()
 export class PostsRepository {
